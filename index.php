9fcd0b8119cddaeb8e3274aeb8746b5ad6714ac1
Jaume <jasato89@gmail.com>
2021-05-05 14:33:01 +0200

    added subjects and tasks files

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Panel - Welcome</title>
 </head>
+
 <body>
-<h1>It works</h1>
-    
+    <?php
+    session_start();
+    if (isset($_SESSION["email"])) {
+        ?>
+        <nav>
+        <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="subjects.php">Asignaturas</a></li>
+        <li><a href="tasks.php">Tareas</a></li>
+        </ul>
+        </nav>
+    <?php
+    } else {
+        header("Location: login/login.php");
+    }
+    ?>
+    <h1>It works</h1>
+
 </body>
+
 </html>
