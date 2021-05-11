5a733b3ce13e8e25be30afc5240fdd23e1463771
Jaume <jasato89@gmail.com>
2021-05-11 10:31:13 +0200

    All data is shown

--- subjects.php
+++ subjects.php
@@ -25,22 +25,56 @@
             $subjects = $database->select("subjects", "*", [
                 "course_id" => $course_code
             ]);
+            printTableHeader();
+            printTable($subjects);
 
-            foreach($subjects as $subject) {
-                echo "<h1>" . $subject["subject_name"] . "<h1>";
-            }
-
+           
         } else {
-            $subjects = $database->select("subjects", "*",[
+            $subjects = $database->select("subjects", "*", [
                 "email" => $_SESSION["email"]
             ]);
             echo "<h1  class=\"pt-12 mt-12\">" . "No hay ningún curso" . "</h1";
-    
-            foreach($subjects as $subject) {
-                echo "<h1>" . $subject["subject_name"] . "<h1>";
-            }   
+
+           printTableHeader();
+           printTable($subjects);
         }
+    } else {
+        header("Location: login/login.php");
+
     }
+
+    function printTableHeader()
+    {
+        echo "<h3>ESTAS SON TODAS TUS ASIGNATURAS</h3>";
+        echo "<table class=\"highlight\">";
+        echo    "<thead>";
+        echo        "<tr>";
+        echo           "<th>Nombre Asignatura</th>";
+        echo            "<th>Número de horas</th>";
+        echo            "<th>Profesor</th>";
+        echo            "<th>Acciones</th>";
+        echo            "<th>Ver Tareas</th>";
+        echo        "</tr>";
+        echo    "</thead>";
+        echo    "<tbody>";
+    }
+
+    function printTable($subjects)
+    {
+        foreach ($subjects as $subject) {
+            echo "<h1>" . $subject["subject_name"] . "<h1>";
+            echo        "<tr>";
+            echo           "<td>" . $subject["subject_name"] . "</td>";
+            echo            "<td>" . $subject["n_hours"] . "</td>";
+            echo            "<td>" . $subject["teacher"] . "</td>";
+            echo            "<td><a href=\"tasks.php?subject=". $subject["idsubjects"] . "\">" . " Ver tareas</a></td>";
+
+            echo        "</tr>";
+        }
+        echo    "</tbody>";
+        echo   " </table>";
+    }
+
     ?>
 
 </body>
--- tasks.php
+++ tasks.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,14 +9,74 @@
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 </head>
+
 <body>
 
-<?php 
+    <?php
+
+    include "connection.php";
+    include "menu.php";
+    session_start();
+
+    if (isset($_SESSION["email"])) {
+        $subject_id = $_GET["subject"];
+        echo "<h1  class=\"pt-12 mt-12\">" . "Tareas" . "</h1";
+
+        if ($subject_id != null) {
+
+            $tasks = $database->select("tasks", "*", [
+                "subject_id" => $subject_id
+            ]);
+            printTaskTableHeader();
+            printTaskTable($tasks);
+        } else {
+
+            $tasks = $database->select("tasks", "*", [
+                "email" => $_SESSION["email"]
+            ]);
+            printTaskTableHeader();
+            printTaskTable($tasks);
+        }
+    } else {
+        header("Location: login/login.php");
+    }
+
+    function printTaskTableHeader()
+    {
+        echo "<table>";
+        echo    "<thead>";
+        echo        "<tr>";
+        echo           "<th>Tarea</th>";
+        echo           "<th>Descripción</th>";
+        echo            "<th>Fecha de Inicio</th>";
+        echo            "<th>Fecha Entrega</th>";
+        echo            "<th>Estado</th>";
+        echo            "<th>Asignatura</th>";
+        echo            "<th>Acciones</th>";
+        echo        "</tr>";
+        echo    "</thead>";
+        echo    "<tbody>";
+    }
+
+    function printTaskTable($tasks)
+    {
+        foreach ($tasks as $task) {
+
+            echo        "<tr>";
+            echo           "<td>" . $task["task_name"] . "</td>";
+            echo           "<td>" . $task["description"] . "</td>";
+            echo            "<td>" . $task["date_start"] . "</td>";
+            echo            "<td>" . $task["date_end"] . "</td>";
+            echo            "<td>" . $task["status"] . "</td>";
+            echo        "</tr>";
+        }
+        echo    "</tbody>";
+        echo   " </table>";
+    }
 
 
+    ?>
 
-
-?>
-    
 </body>
+
 </html>
